14ee666c618dd715f49cde0981a684babff76c9d
Simonjp1 <srrjp123@gmail.com>
2024-08-23 19:41:59 +0200

    added luasnip

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,12 +17,24 @@ return {
 		"neovim/nvim-lspconfig",
 		lazy = false,
 		config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 			local lspconfig = require("lspconfig")
-			lspconfig.html.setup({})
-			lspconfig.lua_ls.setup({})
-			lspconfig.pyright.setup({})
-			lspconfig.clangd.setup({})
-			lspconfig.cssls.setup({})
+			lspconfig.html.setup({
+        capabilities = capabilities
+      })
+			lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+			lspconfig.pyright.setup({
+        capabilities = capabilities
+      })
+			lspconfig.clangd.setup({
+        capabilities = capabilities
+      })
+			lspconfig.cssls.setup({
+        capabilities = capabilities
+      })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
