0334ad4a1786a8379aa0c23f40d9b728bdd33d5d
Simonjp1 <srrjp123@gmail.com>
2024-08-24 10:50:05 +0200

    added autopairs and autotag

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,18 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
+  "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
   config = function()
+    require('nvim-ts-autotag').setup({
+    --   aliases = {
+    --     ["your language here"] = "html",
+    -- }
+    })
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "javascript"},
+      ensure_installed = {"lua", "javascript", "html"},
       highlight = { enable = true },
       indent = { enable = true }
   })
