2943714bc5799c2fa7687201c38efbe16a3625f0
Simon <srrjp123@gmail.com>
2024-08-30 17:30:01 +0200

    added transparency

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,32 +1,59 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-	},
-	{
-		"ellisonleao/gruvbox.nvim",
-		name = "gruvbox",
-		priority = 1000,
-	},
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		priority = 1000,
-	},
-	{
-		"neanias/everforest-nvim",
-		name = "everforest",
-		priority = 1000,
-	},
-	{
-		"scottmckendry/cyberdream.nvim",
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-	},
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                transparent_background = true,  -- Enables transparency for catppuccin theme
+            })
+        end,
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        name = "gruvbox",
+        priority = 1000,
+        config = function()
+            require("gruvbox").setup({
+                transparent_mode = true,  -- Enables transparency for gruvbox theme
+            })
+        end,
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        priority = 1000,
+        config = function()
+            require("rose-pine").setup({
+                disable_background = true,  -- Enables transparency for rose-pine theme
+            })
+        end,
+    },
+    {
+        "neanias/everforest-nvim",
+        name = "everforest",
+        priority = 1000,
+        config = function()
+            vim.g.everforest_background = 'soft'
+            vim.g.everforest_transparent_background = 1  -- Enables transparency for everforest theme
+        end,
+    },
+    {
+        "scottmckendry/cyberdream.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.g.cyberdream_transparent_background = true  -- Enables transparency for cyberdream theme
+        end,
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("tokyonight").setup({
+                transparent = true,  -- Enables transparency for tokyonight theme
+            })
+        end,
+    },
 }
